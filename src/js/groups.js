4a3211b86b8ba6d4f40d3545bc0e981c0b2b66db
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-03-15 14:29:10 -0400

    javascript done

---
+++ src/js/groups.js
@@ -0,0 +1,67 @@
+[
+{
+login: "github",
+id: 9919,
+url: "https://api.github.com/orgs/github",
+repos_url: "https://api.github.com/orgs/github/repos",
+events_url: "https://api.github.com/orgs/github/events",
+hooks_url: "https://api.github.com/orgs/github/hooks",
+issues_url: "https://api.github.com/orgs/github/issues",
+members_url: "https://api.github.com/orgs/github/members{/member}",
+public_members_url: "https://api.github.com/orgs/github/public_members{/member}",
+avatar_url: "https://avatars.githubusercontent.com/u/9919?v=3",
+description: "How people build software."
+},
+{
+login: "libgit2",
+id: 394745,
+url: "https://api.github.com/orgs/libgit2",
+repos_url: "https://api.github.com/orgs/libgit2/repos",
+events_url: "https://api.github.com/orgs/libgit2/events",
+hooks_url: "https://api.github.com/orgs/libgit2/hooks",
+issues_url: "https://api.github.com/orgs/libgit2/issues",
+members_url: "https://api.github.com/orgs/libgit2/members{/member}",
+public_members_url: "https://api.github.com/orgs/libgit2/public_members{/member}",
+avatar_url: "https://avatars.githubusercontent.com/u/394745?v=3",
+description: null
+},
+{
+login: "springtimesoft",
+id: 607604,
+url: "https://api.github.com/orgs/springtimesoft",
+repos_url: "https://api.github.com/orgs/springtimesoft/repos",
+events_url: "https://api.github.com/orgs/springtimesoft/events",
+hooks_url: "https://api.github.com/orgs/springtimesoft/hooks",
+issues_url: "https://api.github.com/orgs/springtimesoft/issues",
+members_url: "https://api.github.com/orgs/springtimesoft/members{/member}",
+public_members_url: "https://api.github.com/orgs/springtimesoft/public_members{/member}",
+avatar_url: "https://avatars.githubusercontent.com/u/607604?v=3",
+description: "Web development and consulting"
+},
+{
+login: "octokit",
+id: 3430433,
+url: "https://api.github.com/orgs/octokit",
+repos_url: "https://api.github.com/orgs/octokit/repos",
+events_url: "https://api.github.com/orgs/octokit/events",
+hooks_url: "https://api.github.com/orgs/octokit/hooks",
+issues_url: "https://api.github.com/orgs/octokit/issues",
+members_url: "https://api.github.com/orgs/octokit/members{/member}",
+public_members_url: "https://api.github.com/orgs/octokit/public_members{/member}",
+avatar_url: "https://avatars.githubusercontent.com/u/3430433?v=3",
+description: null
+},
+{
+login: "todogroup",
+id: 8648704,
+url: "https://api.github.com/orgs/todogroup",
+repos_url: "https://api.github.com/orgs/todogroup/repos",
+events_url: "https://api.github.com/orgs/todogroup/events",
+hooks_url: "https://api.github.com/orgs/todogroup/hooks",
+issues_url: "https://api.github.com/orgs/todogroup/issues",
+members_url: "https://api.github.com/orgs/todogroup/members{/member}",
+public_members_url: "https://api.github.com/orgs/todogroup/public_members{/member}",
+avatar_url: "https://avatars.githubusercontent.com/u/8648704?v=3",
+description: "Talk Openly, Develop Openly"
+}
+]
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
 // Javascript Entry Point
+import $ from 'jquery';
+
+
+
+var url = 'https://api.github.com/users/ammeep/orgs'
+
+
+$.ajax(url).then(function(data) {
+	var $orgs = $('.orgs')
+console.log(data)
+	data.forEach(function({avatar_url, login}) {
+		$orgs.append(`<li>
+						<img src="${avatar_url}">${login}
+					</li>`);
+
+	});
+});
